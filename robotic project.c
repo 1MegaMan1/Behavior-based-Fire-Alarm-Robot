d0571cc21e48868af0b6cfcbaa2d95b823600a18
1MegaMan1 <79240616+1MegaMan1@users.noreply.github.com>
2022-04-20 16:41:19 -0500

    Update robotic project.c

--- robotic project.c
+++ robotic project.c
@@ -15,20 +15,20 @@
 #define SONAR_RANGE_MIN 25
 #define SONAR_FOLLOW_RANGE 5
 #define Wall_THRESHOLD 20
+
 void turnRearMotor(int ROTATION, int DIRECTION);
 void turnFrontMotor(int ROTATION, int DIRECTION);
 void rightTurn(void);
 void leftTurn(void);
-void search(void);
 void forward(int y, int speed);
 void forward_LIGHT(int y, int speed,int light);
 void sonarSTUFF(int sonar_FRONT,int sonar_LEFT,int sonar_RIGHT);
 void Truewander();
 void rndTurn();
 void fix(int sonar_LEFT,int sonar_RIGHT);
-x
+void extinguish ();
 void rightTurn(void)
-{
+{//turns right only during foward locomotion 1 foot foward and one foot right
 	turnFrontMotor(45,ROTATE_RIGHT);
 	turnRearMotor(45,ROTATE_LEFT);
 	forward(1,NORMAL_SPEED);
@@ -36,7 +36,7 @@ void rightTurn(void)
 	turnRearMotor(45,ROTATE_RIGHT);
 }
 void leftTurn(void)
-{
+{//turns left only during foward locomotion approximately 1 foot foward and one foot left
 	turnFrontMotor(45,ROTATE_LEFT);
 	turnRearMotor(45,ROTATE_RIGHT);
 	forward(1,NORMAL_SPEED);
@@ -44,18 +44,8 @@ void leftTurn(void)
 	turnRearMotor(45,ROTATE_LEFT);
 }
 
-void search(void)
-{
-	Off(OUT_AD);
-	RotateMotor(OUT_C,-30,450);
-	Wait(SEC_2);
-	RotateMotor(OUT_C,30,900);
-	Wait(SEC_2);
-	RotateMotor(OUT_C,-30,470);
-	Wait(SEC_2);
-}
 void forward(int y,int speed)
-{
+{//goes foward approximately 1 foot
 	y = y*125;
 	OnFwdSync(OUT_AD,speed);
 	while (0 <= y)
@@ -65,7 +55,7 @@ void forward(int y,int speed)
 	}
 }
 void backward(int y,int speed)
-{
+{//goes backward approximately 1 foot
 	Off(OUT_AD);
 	y = y*125;
 	OnRevSync(OUT_AD,speed);
@@ -77,14 +67,14 @@ void backward(int y,int speed)
 	Off(OUT_AD);
 }
 void turnRearMotor(int ROTATION, int DIRECTION)
-{
+{//turns the holonomic style rear motor
 	ROTATION = ROTATION * 5;// 1 = 5 degrees
 	RotateMotor(OUT_B,50*DIRECTION,ROTATION);
 	//450 is 90 degrees
 	//1800 is full rotation
 }
 void turnFrontMotor(int ROTATION, int DIRECTION)
-{
+{//turns the holonomic style forward motor
 	ROTATION = ROTATION * 5;// 1 = 5 degrees
 	RotateMotor(OUT_C,50*(-DIRECTION),ROTATION);
 	//450 is 90 degrees
@@ -97,7 +87,7 @@ int randomgenerator()
 	return rand() % 10;
 }
 void forward_LIGHT(int y,int speed,int light)
-{
+{//reads the sonar values every millisecond and send the sonar values to fix   
 	int sonar_FRONT = readSensor(IN_1);
 	int sonar_LEFT = readSensor(IN_2);
 	int sonar_RIGHT = readSensor(IN_3);
@@ -150,7 +140,7 @@ void rndTurn()
 	}
 }
 void sonarSTUFF(int sonar_FRONT,int sonar_LEFT,int sonar_RIGHT)
-{
+{//turns left, right, forward, or backward based on already defined thresholds 
 	if(sonar_FRONT < SONAR_RANGE_MIN && sonar_LEFT > SONAR_RANGE_MIN && sonar_RIGHT > SONAR_RANGE_MIN)
 	{
 		rndTurn();
@@ -170,7 +160,8 @@ void sonarSTUFF(int sonar_FRONT,int sonar_LEFT,int sonar_RIGHT)
 		rndTurn();
 	}
 }
-void fix(int sonar_LEFT,int sonar_RIGHT){
+void fix(int sonar_LEFT,int sonar_RIGHT)
+{//fix keeps the robot appromximately 9cm to 20 cm away from the wall on the right or left
 	if(sonar_LEFT > SONAR_FOLLOW_RANGE && sonar_LEFT < SONAR_FOLLOW_RANGE + 4)
 	{
 		//left
@@ -178,13 +169,13 @@ void fix(int sonar_LEFT,int sonar_RIGHT){
 		forward(1 , NORMAL_SPEED);
 		turnFrontMotor(Wall_THRESHOLD, ROTATE_RIGHT);
 	}
-else if(sonar_LEFT < SONAR_FOLLOW_RANGE)
-{
-	turnFrontMotor(Wall_THRESHOLD, ROTATE_RIGHT);
-	forward(1 , NORMAL_SPEED);
-	turnFrontMotor(Wall_THRESHOLD, ROTATE_LEFT);
-}
-if(sonar_RIGHT > SONAR_FOLLOW_RANGE && sonar_RIGHT < SONAR_FOLLOW_RANGE + 4)
+	else if(sonar_LEFT < SONAR_FOLLOW_RANGE)
+	{
+		turnFrontMotor(Wall_THRESHOLD, ROTATE_RIGHT);
+		forward(1 , NORMAL_SPEED);
+		turnFrontMotor(Wall_THRESHOLD, ROTATE_LEFT);
+	}
+	if(sonar_RIGHT > SONAR_FOLLOW_RANGE && sonar_RIGHT < SONAR_FOLLOW_RANGE + 4)
 	{
 		//right
 		turnFrontMotor(Wall_THRESHOLD, ROTATE_RIGHT);
@@ -197,6 +188,15 @@ if(sonar_RIGHT > SONAR_FOLLOW_RANGE && sonar_RIGHT < SONAR_FOLLOW_RANGE + 4)
 		forward(1, NORMAL_SPEED);
 		turnFrontMotor(Wall_THRESHOLD, ROTATE_RIGHT);
 	}
+}
+void extinguish ()
+{//spins for about 5 feet
+	turnFrontMotor(90,ROTATE_LEFT);
+	turnRearMotor(90,ROTATE_RIGHT);
+	forward(5,NORMAL_SPEED);
+	turnFrontMotor(90,ROTATE_RIGHT);
+	turnRearMotor(90,ROTATE_LEFT);
+	
 }
 int main(void)
 {
@@ -224,6 +224,7 @@ int main(void)
 				sonarSTUFF(sonar_FRONT,sonar_LEFT,sonar_RIGHT);
 			}
 		}
+	extinguish();
 	FreeEV3();
 	return 0;
 }
